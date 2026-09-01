8d85ac4115db54a1248a32032684f1d10551972f
Sukriti Srivastava <sukriti.srivastava2903@gmail.com>
2026-09-01 21:42:58 +0530

    service card update

--- src/components/ServiceCard.jsx
+++ src/components/ServiceCard.jsx
@@ -2,19 +2,15 @@ import { Link } from "react-router-dom";
 
 function ServiceCard({ service }) {
   return (
-    <article className="service-card">
-      <div className="service-icon">
-        {service.icon}
-      </div>
+    <Link to="/search" className="service-card">
+      <div className="service-icon">{service.icon}</div>
 
       <h3>{service.name}</h3>
 
       <p>{service.description}</p>
 
-      <Link to="/search" className="card-link">
-        Find care →
-      </Link>
-    </article>
+      <span>Explore →</span>
+    </Link>
   );
 }
 
