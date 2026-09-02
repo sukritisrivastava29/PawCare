d3e15898993fe7e254f98a4980a576b3203f2bae
Sukriti Srivastava <sukriti.srivastava2903@gmail.com>
2026-09-02 22:09:29 +0530

    feat: implement JWT authentication and protected routes

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,12 +1,23 @@
 const express = require("express");
+
 const {
   registerUser,
   loginUser,
 } = require("../controllers/authController");
 
+const protect = require("../middleware/authMiddleware");
+
 const router = express.Router();
 
 router.post("/register", registerUser);
+
 router.post("/login", loginUser);
+
+router.get("/me", protect, (req, res) => {
+  res.json({
+    message: "Protected route working",
+    user: req.user,
+  });
+});
 
 module.exports = router;
