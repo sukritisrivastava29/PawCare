a21ccf0784fde7b050f79bd062a5c5cc4dce2886
Sukriti Srivastava <sukriti.srivastava2903@gmail.com>
2026-09-02 16:03:03 +0530

    server update

---
+++ server/routes/aiRoutes.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const {
+  chatWithAI,
+} = require("../controllers/aiController");
+
+const router = express.Router();
+
+router.post("/chat", chatWithAI);
+
+module.exports = router;
